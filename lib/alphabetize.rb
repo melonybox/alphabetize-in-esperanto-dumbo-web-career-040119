f2efd9229c507dba0c34d63f5bef8c8f618ff62d
Gino Desangles <blushingbox@yahoo.com>
2019-03-27 06:02:15 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -7,19 +7,7 @@ def alphabetize(arr)
   newListA = arr
   newListB = []
   
-  newListA.sort
   
-  newList.each do |x|
-    counter = 0
-    counterDeux = 0
-    
-    newListA.each do |y|
-      if y[counter] == x
-        newListB << y
-      end
-    end
-    counter += 1
-  end
 end
 
 newListA.each do |y|
