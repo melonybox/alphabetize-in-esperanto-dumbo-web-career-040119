a623584bb6f642e9f59a77b4becfa0e7b1ab10e7
Gino Desangles <blushingbox@yahoo.com>
2019-03-27 06:45:49 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+  newList = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
+  
+  newListA = arr
+  newListB = []
+  
+  newListA.sort!
+  
+  newList.each do |x|
+    newListA.each do |y|
+      if x == y[0]
+        newListB << y
+      end
+    end
+  end
+  
+  return newListB
+  
 end
