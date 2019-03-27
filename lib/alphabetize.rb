9b210c7e3faced64277d4d50c391749073e5078c
Gino Desangles <blushingbox@yahoo.com>
2019-03-27 05:56:51 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -12,9 +12,7 @@ def alphabetize(arr)
   newList.each do |x|
     counter = 0
     counterDeux = 0
-    if newListA.size == newListB.size
-      return newListB
-    end
+    
     newListA.each do |y|
       if y[counter] == x
         newListB << y
